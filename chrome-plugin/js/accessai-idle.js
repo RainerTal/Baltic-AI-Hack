a09e74ff7061e69eb0544e14c409e5bcb2b55b4d
wilpeers <wilpeers@gmail.com>
2024-10-05 04:18:06 +0300

    fix 403 error loosing the function definition..

--- chrome-plugin/js/accessai-idle.js
+++ chrome-plugin/js/accessai-idle.js
@@ -2,6 +2,18 @@
 function speakText() {
 (window.speechSynthesis || window.webkitSpeechSynthesis).speak(new SpeechSynthesisUtterance("Hello Hack AI! Nice to meet you!"))
 }
+console.log(document);
+
+if(chrome.runtime){
+const script = document.createElement('script');
+script.src = chrome.runtime.getURL('js/accessai-idle.js');
+(document.head || document.documentElement).appendChild(script);
+script.onload = function() {
+    script.remove();
+};
+}
+
+//setTimeout((fn)=> {if(!speakText) speakText = fn}, 1000,  speakText)
 
 if(!document.querySelector("button.AccessAI-trigger")) document.body.insertAdjacentHTML( 'beforeend', "<button onclick=\"speakText()\" class=\"AccessAI-trigger\">Speak</button>")
 //window.onload = () => document.querySelector("button").click();
